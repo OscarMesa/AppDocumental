ba04b4b8392293216137f3e4fd5d58208c831dec
oskar <oskar@oskar-ThinkPad-R400>
2014-05-16 12:55:36 -0500

    cambios ...

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -1,9 +1,12 @@
 <?php /* @var $this Controller */ ?>
-<?php $this->beginContent('//layouts/main'); ?>
+<?php $this->beginContent('//layouts/main');
+?>
 <div class="span-19">
-    <div id="content" class="seccion-1">
-        <?php echo $content; ?>
-    </div><!-- content -->
+    <div id="content" class="<?php echo strpos(Yii::app()->controller->getRoute(),'documento') !== false?'seccion-1':'';?>">
+        <?php
+            echo $content; 
+        ?>
+    </div>
 </div>
 <div id="menu-lateral" class="span-5">
         <?php
